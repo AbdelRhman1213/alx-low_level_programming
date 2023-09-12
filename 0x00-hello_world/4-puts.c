55fef31042a3b26a25ef0aa86db4fb89044a97dc
Abdo_Sayed <abdosayed1213@gmail.com>
2023-09-11 23:07:00 -0700

    a

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-int main ()
+
+/*
+ * main -Entry point
+ */
+int main(void)
 {
-  pusts("Programming is like building a multilingual puzzle\n");
-  return 0;
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
 }
