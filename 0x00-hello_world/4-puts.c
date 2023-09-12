c9180008fa85b83026b046645274eb49c690860a
Abdo_Sayed <abdosayed1213@gmail.com>
2023-09-11 23:11:11 -0700

    a

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,11 @@
 
 /*
  * main -Entry point
+ * Describtion : A C programming that print with puts function
+ * Return : always 0 (succsess)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
