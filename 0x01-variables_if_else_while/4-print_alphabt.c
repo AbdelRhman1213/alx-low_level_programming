c20bf71e5d9c629df2b9f542c14fa13f844fe106
Abdo_Sayed <abdosayed1213@gmail.com>
2023-09-14 09:28:27 -0700

    a

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description for function main : false or true validation
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch != 'q' || ch != 'e')
+		putchar(ch);
+		ch++;
+	}
+
+	putchar('n');
+}
