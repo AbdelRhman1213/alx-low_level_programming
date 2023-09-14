5f0a29c5e2087637ddec12f9268adcaf4a4d9bdc
Abdo_Sayed <abdosayed1213@gmail.com>
2023-09-14 09:55:44 -0700

    a

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description for function main : false or true validation
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char num = '0';
+
+	while (num <= '9')
+	{
+		putchar(num);
+		num++;
+	}
+	putchar('\n');
+
+	return (0);
+}
