11e4ed3fa024c3b8a7ea93b5a5182b769dab1587
Abdo_Sayed <abdosayed1213@gmail.com>
2023-09-15 17:41:41 -0700

    a

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * more_numbers -print from 1 to 14 (10) times
+ *
+ *
+ *
+ * Return: 1 if its upper, 0 if not
+ */
+void more_numbers(void)
+{
+	int i, a, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (a = 0; a <= 14; a++)
+		{
+			a = j;
+
+			if (j > 9)
+			{
+				_putchar(1 + 48);
+				j = a % 10;
+			}
+			_putchar(a + 48);
+		}
+		_putchar('\n');
+	}
+}
+
