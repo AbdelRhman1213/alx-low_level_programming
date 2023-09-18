ac6e23f5e03c9d0d0d60df22b3fa5640fc033eca
Abdo_Sayed <abdosayed1213@gmail.com>
2023-09-18 16:42:57 -0700

    a

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/*
+ * *_strcpy - takes a pointer to an int parameter and update its value to 98
+ *
+ * @dest: int parameter
+ * @src: int parameter
+ *
+ * Return: array of dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = -1;
+
+	do {
+		i++;
+		dest[i] = src[i];
+	} while (src[i] != '\0');
+
+	return (dest);
+}
