7dda5f486a8db178f9eb8676db7ea0da0885dbb6
Abdo_Sayed <abdosayed1213@gmail.com>
2023-09-18 16:05:42 -0700

    a

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - takes a pointer to an int parameter and update its value to 98
+ *
+ * @str: int parameter
+ *
+ * return: nothing
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}	
