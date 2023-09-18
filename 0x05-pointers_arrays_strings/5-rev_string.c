0030e724bd1ac5bc184a1f73b8add53d361a4361
Abdo_Sayed <abdosayed1213@gmail.com>
2023-09-18 15:49:25 -0700

    a

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string - takes a pointer to an int parameter and
+ * * @s: int parameter
+ *
+ * return: nothing
+ */
+
+void rev_string(char *s)
+{
+	int l, i;
+	char t;
+
+	while (s[i])
+		i++;
+
+	for (i = 0; i < l / 2; i++)
+	{
+		t = s[i];
+		s[i] = s[l - 1 - i];
+		s[i] = t;
+	}
+}
