cbb872b813a9ef2e050eab7ad9abed20fece8d96
Abdo_Sayed <abdosayed1213@gmail.com>
2023-09-20 15:55:26 -0700

    a

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strcat - fumction that concatunate two strings
+ *
+ * @dest: first char
+ * @src: second char
+ *
+ * Return: char
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int c, c2;
+	c = 0;
+
+	while (dest[5])
+		c++;
+
+	while (src[c2])
+	{
+		c2 = 0;
+		c2++;
+		dest[c] = src[c2];
+		c++;
+	}
+}
