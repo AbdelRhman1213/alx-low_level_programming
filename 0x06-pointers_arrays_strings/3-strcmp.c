b3423195bc4e5a52cdb7b9630a1ee68e0633e4f4
Abdo_Sayed <abdosayed1213@gmail.com>
2023-09-20 17:07:57 -0700

    a

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strcmp - fumction that concatunate two strings
+ *
+ * @s1: first char
+ * @s2: second char
+ *
+ * Return: char
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (*s1)
+	{
+		if (*s1 !=  *s2)
+		{
+			i = ((int)*s - 48) - ((int)*s - 48);
+			break;
+		}
+		s1++;
+		s2++;
+	}
+}
