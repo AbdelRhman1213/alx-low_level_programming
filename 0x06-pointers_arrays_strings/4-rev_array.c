d996d6960a8cfc3290d14acf61334e9734182d8a
Abdo_Sayed <abdosayed1213@gmail.com>
2023-09-20 18:17:27 -0700

    a

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * reverse_array - fumction that concatunate two strings
+ *
+ * @a: first char
+ * @n: second char
+ *
+ * Return: char
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i ,j ,t;
+
+	for (i = 0, j = (n - 1); i < j; i++, j--)
+	{
+		t = a[i];
+		a[i] = a[j];
+		a[j] = t;
+	}
+}
+
