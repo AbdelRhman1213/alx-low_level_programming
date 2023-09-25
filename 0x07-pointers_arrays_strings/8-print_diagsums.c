62a4c9888dbcce1b8e3dcbb89108fb9e7d390121
Abdo_Sayed <abdosayed1213@gmail.com>
2023-09-25 13:28:01 -0700

    a

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - fumction that concatunate two strings
+ *
+ * @size: first char
+ * @a: second char
+ * 
+ *
+ * Return: char
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i , s1 = 0, s2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		s1 += a[i];
+		s2 += a[size - i - 1];
+		a += size;
+	}
+	printf("%d", s1);
+	printf("%d\n", s2);
+}
