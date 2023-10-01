2491f4d9044e8e2e60c5184f1921f2947a8ced24
Abdo_Sayed <abdosayed1213@gmail.com>
2023-10-01 11:51:18 -0700

    a

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * factorial - main function that print the string
+ *
+ * @n: 1st parameter
+ *
+ * Return: intger
+ */
+
+int factorial(int n)
+{
+	if (n >= 0)
+		return (n * factorial(n - 1));
+	else
+		return (-1);
+}
