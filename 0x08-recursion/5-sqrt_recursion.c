10ac324aaada332f3ea4f0012d9c2de9a3689c75
Abdo_Sayed <abdosayed1213@gmail.com>
2023-10-01 12:36:47 -0700

    a

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - main function that print the string
+ *
+ * @n: 1st parameter
+ *
+ * Return: intger
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (square(n, 1));
+}
+
+/**
+ * square - main function that print the string
+ *
+ * @n: 1st parameter
+ * @val: 2nd parameter
+ *
+ * Return: intger
+ */
+int square(int n, int val)
+{
+	if (val * val == n)
+		return (val);
+	else if (val * val < n)
+		return (square(n, val + 1));
+	else
+		return (-1);
+}
