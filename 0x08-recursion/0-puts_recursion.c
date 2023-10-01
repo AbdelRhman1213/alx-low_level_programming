456b885c0cf9ba4263fa1e9d7664a63df8014321
Abdo_Sayed <abdosayed1213@gmail.com>
2023-10-01 10:30:07 -0700

    a

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,4 +1,4 @@
-#include "main"
+#include "main.h"
 
 /**
  * _puts_recursion - main function that print the string
@@ -8,12 +8,13 @@
  * Return: void
  */
 
+
 void _puts_recursion(char *s)
 {
 	if (*s != '\0')
 	{
 		_putchar(*s);
-		_puts_recursion(s + 1)
+		_puts_recursion(s + 1);
 	}
 	else
 	{
