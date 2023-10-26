5211969d12fdd1a840aaaf2de99eb3c3daf1a497
Abdo_Sayed <abdosayed1213@gmail.com>
2023-10-26 02:19:02 -0700

    a

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -35,5 +35,12 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int get_endianness(void);
 
 #endif
