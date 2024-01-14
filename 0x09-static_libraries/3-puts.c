1ec8fbdc2bc751e98e2c6f179b6773580767dd7c
root <abdosayed1213@gmail.com>
2024-01-14 03:51:29 -0800

    a

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+/**
+ * _puts - Entry point
+ *
+ *@s: afsgcgh
+ *
+ * Return: Always 0 (Succes)
+ */
+void _puts(char *s)
+{
+	puts(s);
+}
+
